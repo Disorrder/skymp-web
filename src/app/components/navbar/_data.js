412bf96e96573a615042689d2fbcbbf03557b9ee
Disorder <disorrder@gmail.com>
2018-02-11 19:51:07 +0300

    hover effects

--- src/app/components/navbar/_data.js
+++ src/app/components/navbar/_data.js
@@ -3,9 +3,9 @@ module.exports = {
         {name: 'main', url: '/', title: 'Новости'},
         {name: 'forum', url: 'http://skymp.ru/forum/', title: 'Форум'},
         {name: 'about', url: '/about', title: 'Особенности'},
-        {name: 'support', url: '#support', title: 'Поддержка'},
-        {name: 'shop', url: '#shop', title: 'Магазин'},
-        {name: 'profile', url: '#profile', title: 'Профиль'}
+        {name: 'support', url: '#support', title: 'Поддержка', enabled: false},
+        {name: 'shop', url: '#shop', title: 'Магазин', enabled: false},
+        {name: 'profile', url: '#profile', title: 'Профиль', enabled: false}
     ]
 };
 
