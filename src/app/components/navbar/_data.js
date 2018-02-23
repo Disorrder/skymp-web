945c0ca339200350c52b74ae50a82d6f1ace9830
Disorder <disorrder@gmail.com>
2018-02-23 15:06:16 +0300

    navbar root

--- src/app/components/navbar/_data.js
+++ src/app/components/navbar/_data.js
@@ -1,17 +1,19 @@
+var root = '/';
+var useIndex = false;
+
+function getUrl(route) {
+    var url = root + route;
+    if (useIndex) url += '/index.html';
+    return url;
+}
+
 module.exports = {
     menu: [
-        {name: 'main', url: '/', title: 'Новости'},
+        {name: 'main', url: root, title: 'Новости'},
         {name: 'forum', url: 'http://skymp.ru/forum/', title: 'Форум'},
-        {name: 'about', url: '/about', title: 'Особенности'},
+        {name: 'about', url: getUrl('about.html'), title: 'Особенности'},
         {name: 'support', url: '#support', title: 'Поддержка', enabled: false},
         {name: 'shop', url: '#shop', title: 'Магазин', enabled: false},
         {name: 'profile', url: '#profile', title: 'Профиль', enabled: false}
     ]
 };
-
-const staticPages = true;
-if (staticPages) {
-    module.exports.menu.forEach((v) => {
-        if (v.url[0] === '/' && v.url.length > 1) v.url += '.html';
-    });
-}
