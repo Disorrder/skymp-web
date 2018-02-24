ae65303087440b2eba2110eb7b5b84248354463b
Disorder <disorrder@gmail.com>
2018-02-24 12:17:23 +0300

    main - media source

--- src/app/pages/main/_data.js
+++ src/app/pages/main/_data.js
@@ -9,26 +9,38 @@ module.exports = {
     ],
 
     carouselItems: [
-        {url: '#', img: require("assets/images/carousel/carousel-1.png")},
-        {url: '#', img: require("assets/images/carousel/carousel-1.png")},
-        {url: '#', img: require("assets/images/carousel/carousel-1.png")}
+        // {url: '#', img: require("assets/images/carousel/DutIHtklYcU.jpg")},
+        // {url: '#', img: require("assets/images/carousel/ETS22p7alP0.jpg")},
+        // {url: '#', img: require("assets/images/carousel/gE8-nn3THAM.jpg")},
+        // {url: '#', img: require("assets/images/carousel/hITL_GtkeUo.jpg")},
+        // {url: '#', img: require("assets/images/carousel/L3m766qk3f8.jpg")},
+        // {url: '#', img: require("assets/images/carousel/trVfZx2rMV8.jpg")}
     ],
 
     articles: [
         {
-            id: 1,
-            date: '01.01.2018',
-            title: 'Дневник разработчика SkyMP v.0.9: Синхронизация.',
-            video_yt: '1hfcw74tV8U'
-        }
+            date: '30.11.2017.',
+            title: 'Дневник разработчика SkyMP v.0.9: Синхронизация',
+            media: { source: 'youtube', id: '1hfcw74tV8U' },
+        },
+        {
+            date: '17.12.2017',
+            title: 'Дневник разработчика SkyMP v. 0.11: Крафт',
+            media: { source: 'youtube', id: 'e9Ipv-9NS9M' },
+        },
+        {
+            date: '31.12.2017',
+            title: 'Дневник разработчика SkyMP v. 0.14: NPC',
+            media: { source: 'youtube', id: 'lMM3AV3mX40' },
+        },
+        {
+            date: '23.02.2018',
+            title: 'Дневник разработчика SkyMP v. 1.0',
+            media: { source: 'youtube', id: 'F9CHiCXR5NU' },
+        },
     ]
 };
 
-// for (let i=1; i <5; i++) {
-//     module.exports.articles.push({
-//         id: i,
-//         title: 'Title '+i,
-//         lead: 'Lead '+i,
-//         fullText: 'Full text '+i
-//     });
-// }
+module.exports.articles.forEach((v, i) => {
+    v.id = i+1;
+});
