e6eca94ed6358dc79e3445a3cf812c90fe284169
Disorder <disorrder@gmail.com>
2018-03-20 15:41:24 +0300

    OG meta, nav fix

--- src/app/components/navbar/_data.js
+++ src/app/components/navbar/_data.js
@@ -1,17 +1,11 @@
 var root = '/';
 var useIndex = false;
 
-function getUrl(route) {
-    var url = root + route;
-    if (useIndex) url += '/index.html';
-    return url;
-}
-
 module.exports = {
     menu: [
         {name: 'main', url: root, title: 'Новости'},
-        {name: 'forum', url: 'http://skymp.ru/forum/', title: 'Форум'},
-        {name: 'about', url: getUrl('about.html'), title: 'Особенности'},
+        {name: 'forum', url: 'http://forum.skymp.ru/', title: 'Форум'},
+        {name: 'about', url: '/about.html', title: 'Особенности'},
         {name: 'support', url: '#support', title: 'Поддержка', enabled: false},
         {name: 'shop', url: '#shop', title: 'Магазин', enabled: false},
         {name: 'profile', url: '#profile', title: 'Профиль', enabled: false}
