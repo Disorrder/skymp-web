306e87aa9c8e184b46a063dc31efec8e30b78063
Disorder <disorrder@gmail.com>
2018-04-13 14:23:19 +0300

    fix port on staging

--- src/config.default.js
+++ src/config.default.js
@@ -2,3 +2,7 @@ window.config = {
     // api: '/api',
     api: `//${location.hostname}:2000`,
 };
+
+if (location.hostname !== 'localhost') {
+    config.api = config.api.replace(':2000', ':2001');
+}
