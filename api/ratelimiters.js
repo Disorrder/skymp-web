080a8575b845022994170aa7ffbfa56949455901
Shivanar <shivanar@yandex.ru>
2018-05-08 22:14:08 +0000

    Api: config option to disable redis

--- api/ratelimiters.js
+++ api/ratelimiters.js
@@ -1,23 +1,29 @@
 const cfg = require('./config');
-const ratelimit = require('koa-ratelimit');
-const Redis = require('ioredis');
 
-// documentation: https://github.com/koajs/ratelimit
+if (cfg.ratelimitEnabled) {
+    // documentation: https://github.com/koajs/ratelimit
+    let ratelimit = require('koa-ratelimit');
+    let Redis = require('ioredis');
 
-module.exports.global = ratelimit({
-    db: new Redis(cfg.redis + '/1'),
-    duration: 60000,
-    errorMessage: '<h1>Error 429: Too many requests!</h1>',
-    id: (ctx) => ctx.ip,
-    max: 100,
-    disableHeader: true,
-});
+    module.exports.global = ratelimit({
+        db: new Redis(cfg.redis + '/1'),
+        duration: 60000,
+        errorMessage: '<h1>Error 429: Too many requests!</h1>',
+        id: (ctx) => ctx.ip,
+        max: 100,
+        disableHeader: true,
+    });
 
-module.exports.auth = ratelimit({
-    db: new Redis(cfg.redis + '/2'),
-    duration: 60000,
-    errorMessage: '<h1>Error 429: Too many requests!</h1>',
-    id: (ctx) => ctx.ip,
-    max: 5,
-    disableHeader: true,
-});
+    module.exports.auth = ratelimit({
+        db: new Redis(cfg.redis + '/2'),
+        duration: 60000,
+        errorMessage: '<h1>Error 429: Too many requests!</h1>',
+        id: (ctx) => ctx.ip,
+        max: 5,
+        disableHeader: true,
+    });
+} else {
+    let idle = async (ctx, next) => await next();
+    module.exports.global = idle;
+    module.exports.auth = idle;
+}
