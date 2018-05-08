0c99136b22d20df84b53c58db1eef1b2f84b9e62
Shivanar <shivanar@yandex.ru>
2018-05-08 22:48:23 +0300

    Config update and code refactoring

--- api/config.default.js
+++ api/config.default.js
@@ -2,6 +2,7 @@ module.exports = {
     "secretKey": "WhiskeySexAndRocknroll",
     // "db": "mongodb://admin:adminpwd@ds135186.mlab.com:35186/skymp-test",
     "db": "mongodb://admin:adminpwd@ds014578.mlab.com:14578/skymp-test-azure",
+    "redis": "redis://127.0.0.1:6379",
     "mailgun": {
         "domain": "mail.skymp.ru",
         "apiKey": "key-bca7a387d9fdd82523b70a6fec2dfa26",
--- api/ratelimiters.js
+++ api/ratelimiters.js
@@ -1,28 +1,23 @@
 const cfg = require('./config');
-const Ratelimit = require('koa-ratelimit');
+const ratelimit = require('koa-ratelimit');
 const Redis = require('ioredis');
 
-let redisIndex = 1;
+// documentation: https://github.com/koajs/ratelimit
 
-let ratelimiters = {
-    // documentation: https://github.com/koajs/ratelimit
+module.exports.global = ratelimit({
+    db: new Redis(cfg.redis + '/1'),
+    duration: 60000,
+    errorMessage: '<h1>Error 429: Too many requests!</h1>',
+    id: (ctx) => ctx.ip,
+    max: 100,
+    disableHeader: true,
+});
 
-    global: Ratelimit({
-        db: Redis(cfg.redis + '/' + redisIndex++),
-        duration: 60000,
-        errorMessage: '<h1>Error 429: Too many requests!</h1>',
-        id: (ctx) => ctx.ip,
-        max: 100,
-        disableHeader: true,
-    }),
-    auth: Ratelimit({
-        db: Redis(cfg.redis + '/' + redisIndex++),
-        duration: 60000,
-        errorMessage: '<h1>Error 429: Too many requests!</h1>',
-        id: (ctx) => ctx.ip,
-        max: 5,
-        disableHeader: true,
-    })
-}
-
-module.exports = ratelimiters;
+module.exports.auth = ratelimit({
+    db: new Redis(cfg.redis + '/2'),
+    duration: 60000,
+    errorMessage: '<h1>Error 429: Too many requests!</h1>',
+    id: (ctx) => ctx.ip,
+    max: 5,
+    disableHeader: true,
+});
