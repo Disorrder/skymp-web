2c0270aec94c5950932a8ff8034a3fab4f27d383
Disorder <disorrder@gmail.com>
2018-05-10 01:24:43 +0300

    fix config

--- api/config.default.js
+++ api/config.default.js
@@ -1,7 +1,7 @@
 module.exports = {
     "secretKey": "WhiskeySexAndRocknroll",
-    // "db": "mongodb://admin:adminpwd@ds135186.mlab.com:35186/skymp-test",
-    "db": "mongodb://admin:adminpwd@ds014578.mlab.com:14578/skymp-test-azure",
+    // "db": "mongodb://admin:adminpwd@ds014578.mlab.com:14578/skymp-test-azure",
+    "db": "mongodb://admin:adminpwd@ds119150.mlab.com:19150/skymp-web-test",
     "ratelimitEnabled": false,
     "redis": "redis://127.0.0.1:6379",
     "mailgun": {
